e50addb5b7fb994ca113f0267153efe5e0c16d67
Cal Davis <cad0031@gwu.edu>
2024-03-27 21:19:42 -0400

    Final data set with offset rows for model execution

--- Analysis files/Model Execution.py
+++ Analysis files/Model Execution.py
@@ -1,6 +1,7 @@
 import pandas
 
-df = pandas.read_csv("Documents/GitHub/Capstone-project/Data folder/final_dataset.csv")
-print(df.head(10))
+#df = pandas.read_csv("Documents/GitHub/Capstone-project/Data folder/final_dataset.csv")
+#print(df.head(10))
 
 print("Done")
+
